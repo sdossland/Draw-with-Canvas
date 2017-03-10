f596b312294c9980b7e6ade734acb6262c47dbad
Sophia Dossland <sophia.dossland@gmail.com>
2017-03-09 16:16:17 -0800

    update layout, drawing capability with variable line width and colors

--- logic.js
+++ logic.js
@@ -5,29 +5,70 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const canvas = document.querySelector('.board');
   const context = canvas.getContext('2d'); //returns a drawing context on a canvas
-  context.strokeStyle = '#00522c'; //sets the color of the strokes
+
+  let drawing = false; //initial value, that changes to true upon 'mousedown'
+  //defines starting coordinates
+  let startX = 0;
+  let startY = 0;
+
+  //line specifics
+  let lineWidth = 30; //initial width
+  let lineWidthDescending = true; //
+  let hue = 0; //variable color to be applied
   context.lineJoin = 'round'; //sets the style for when two lines meet
   context.lineCap = 'round'; //sets the style for the start and end caps of a line
 
-  let drawing = false; //initial value, that changes to true upon 'mousedown'
-  let lastX = 0;
-  let lastY = 0;
+  function lineWidthControl() {
+    if (lineWidthDescending) {
+      if (lineWidth > 5) {
+        lineWidth -= 0.2;
+      } else {
+        lineWidthDescending = !lineWidthDescending;
+      }
+    } else if (!lineWidthDescending) {
+      if (lineWidth < 30) {
+        lineWidth += 0.2;
+      } else {
+        lineWidthDescending = !lineWidthDescending;
+      }
+    }
+  }
 
   function draw(e) {
     if (!drawing) { //kills the function if not drawing
       return;
     }
     context.beginPath();
-    context.moveTo(lastX, lastY);
-    context.lineTo(e.offsetX, e.offsetY);
+    context.moveTo(startX, startY); //sets a start position but does NOT create a line
+    context.lineTo(e.offsetX, e.offsetY); //creates a line from last specified pt to this pt, but does NOT draw line
+    context.stroke(); //draws the line
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`; //sets the color of the strokes
+    context.lineWidth = `${lineWidth}`; //sets the width of the strokes
+
+    //keeps resetting the start coordinates to allow for a long, curvy line
+    startX = e.offsetX;
+    startY = e.offsetY;
+
+    lineWidthControl(); //change the line width with each new set of coordinates
+
+    hue++; //increases the hue with every new coordinate to achieve full spectrum
+    //once full spectrum is reached (aka 360), reset to 0 to start spectrum over
+    if (hue == 360) {
+      hue = 0;
+    }
+
   }
 
 
-  canvas.addEventListener('mousemove', draw);
-  //on click initiate drawing
-  canvas.addEventListener('mousedown', function() {
-    return drawing = true;
+
+  //on click initiate drawing and capture start coordinates
+  canvas.addEventListener('mousedown', (e) => {
+      drawing = true;
+      startX = e.offsetX;
+      startY = e.offsetY;
   });
+  //draws line while moving mouse
+  canvas.addEventListener('mousemove', draw);
   //stop drawing upon release
   canvas.addEventListener('mouseup', function() {
     return drawing = false;
@@ -36,6 +77,5 @@ document.addEventListener("DOMContentLoaded", function () {
   canvas.addEventListener('mouseout', function() {
     return drawing = false;
   });
-
 
 });
